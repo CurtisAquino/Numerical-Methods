de977238d918c37d3c16148f7446c37e890b5da3
Curtis Aquino <curtaquino@gmail.com>
2019-03-11 19:24:36 -0400

    Update Interpolation.m

--- Interpolation.m
+++ Interpolation.m
@@ -17,22 +17,21 @@ function F = PiecewiseLinearInterpolation(x,y,x0)
     m       = length(x0);
     a       = (y(2:n) - y(1:(n-1)))./(x(2:n)-x(1:(n-1)));
     b       = (y(1:(n-1)).*x(2:n)-y(2:n).*x(1:(n-1)))./(x(2:n)-x(1:(n-1)));
-    
-for i = 1:m
-    idx     = find(bds(:,1) <= x0(i) & x0(i) <= bds(:,2));
-    if length(idx) > 1
-        idx     = find(bds(:,1) <= x0(i) & x0(i) < bds(:,2));
+    for i = 1:m
+        idx     = find(bds(:,1) <= x0(i) & x0(i) <= bds(:,2));
+        if length(idx) > 1
+            idx     = find(bds(:,1) <= x0(i) & x0(i) < bds(:,2));
+        end
+        if isempty(idx)
+            if x0(i) < x(1)
+                F(i) = x(1);
+            elseif x0(i) > x(end)
+                F(i) = x(end);
+            end
+        else
+            F(i)    = a(idx)*x0(i)+b(idx);
+        end
     end
-    if isempty(idx)
-        if x0(i) < x(1)
-            F(i) = x(1);
-        elseif x0(i) > x(end)
-            F(i) = x(end);
-        end    
-    else 
-        F(i)    = a(idx)*x0(i)+b(idx);
-    end
-end
 end
 
     end
